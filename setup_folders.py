467047d46cd9684ed49b7e3716e0beb8a8b7b6ba
NabidAlam <nabid.aust37@gmail.com>
2025-08-12 21:38:37 +0200

    Commit changes to setup_folders.py

--- setup_folders.py
+++ setup_folders.py
@@ -6,7 +6,7 @@ base_output = 'output'
 
 for d in datasets:
     os.makedirs(os.path.join(base_video, d), exist_ok=True)
-    os.makedirs(os.path.join(base_output, 'tsp_sam', d), exist_ok=True)
+    os.makedirs(os.path.join(base_output, 'tspsam', d), exist_ok=True)
     os.makedirs(os.path.join(base_output, 'samurai', d), exist_ok=True)
 
 # python setup_folders.py
